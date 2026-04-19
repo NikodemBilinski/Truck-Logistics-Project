ba8f7a9128585975df8235430da6f6a2bc0880b0
Nikodemus <nikodembilinski4@gmail.com>
2026-04-19 19:00:12 +0200

    added creating new trucks in swagger for now along with some console logs

--- Truck Logistics Server ASP.NET/Controllers/ValuesController.cs
+++ Truck Logistics Server ASP.NET/Controllers/ValuesController.cs
@@ -23,7 +23,24 @@ namespace TrucksLogisticsServerAPI.Controllers
 
         public async Task <ActionResult<List<Truck>>> GetTrucks()
         {
+            Console.WriteLine("GetTrucks Requested");
+
             return Ok(await _dataContext.Trucks.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Truck>> AddTruck(Truck truck)
+        {
+            
+            _dataContext.Trucks.Add(truck);
+
+            await _dataContext.SaveChangesAsync();
+
+            Console.WriteLine("Truck added: " + truck.Id + ", " + truck.Owner + ", " + truck.IsBusy + ", " + truck.Capacity);
+
+            return Ok(await _dataContext.Trucks.ToListAsync());
+
+            
         }
     }
 }
