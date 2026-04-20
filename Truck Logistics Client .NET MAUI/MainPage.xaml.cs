a5b129214ad4117be32ed6cee02852e20387787d
Nikodemus <nikodembilinski4@gmail.com>
2026-04-20 13:39:24 +0200

    added hardcoded login system with fake token in response

--- Truck Logistics Client .NET MAUI/MainPage.xaml.cs
+++ Truck Logistics Client .NET MAUI/MainPage.xaml.cs
@@ -21,16 +21,38 @@ namespace TrucksLogisticsClient
 
             var response = await client.GetFromJsonAsync<List<Truck>>(url);
 
-            if(response != null)
+            if (response != null)
             {
                 TrucksLabel.Text = string.Empty;
-                foreach(var truck in response)
+                foreach (var truck in response)
                 {
                     TrucksLabel.Text += truck.Id + " " + truck.Owner + " " + truck.Capacity + " " + truck.IsBusy + "\n";
                 }
             }
 
+        }
 
+        private async void Login_Clicked(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+
+            var url = "http://192.168.0.218:5160/api/Auth/Login";
+
+            var response = await client.PostAsJsonAsync(url, new { Username = Login_entry.Text.ToString(), Password = Password_entry.Text.ToString() });
+
+            if (response.IsSuccessStatusCode)
+            {
+                var tokenResponse = await response.Content.ReadAsStringAsync();
+                if (tokenResponse != null)
+                {
+                    LoginResultLabel.Text = "Login successful! Token: " + tokenResponse;
+                    await SecureStorage.Default.SetAsync("AuthToken", tokenResponse);
+                }
+            }
+            else
+            {
+                LoginResultLabel.Text = "Login failed!";
+            }
         }
     }
 }
---
+++ Truck Logistics Server ASP.NET/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrucksLogisticsServerAPI.Models;
+
+namespace TrucksLogisticsServerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody] LoginModel model)
+        {
+            Console.WriteLine("Request to login for user: " + model.Username);
+            // For demonstration purposes, we will use hardcoded credentials.
+            // In a real application, you should validate against a database or an identity provider.
+            if (model.Username == "admin" && model.Password == "admin")
+            {
+                Console.WriteLine("Login successful for user: " + model.Username);
+                var token = "fake-jwt-token-for-admin";
+                return Ok(new { Token = token });
+            }
+            else
+            {
+                Console.WriteLine("Login unsuccessful for user: " + model.Username);
+                return Unauthorized();
+            }
+        }
+    }
+}
