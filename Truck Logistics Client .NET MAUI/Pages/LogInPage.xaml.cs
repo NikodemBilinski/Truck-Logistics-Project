3871db9fdd71cd6858e3b4336056991808118ce2
Nikodemus <nikodembilinski4@gmail.com>
2026-04-20 20:42:37 +0200

    feature: added error border to show error for users in loginpage

--- Truck Logistics Client .NET MAUI/Pages/LogInPage.xaml.cs
+++ Truck Logistics Client .NET MAUI/Pages/LogInPage.xaml.cs
@@ -17,45 +17,87 @@ namespace TrucksLogisticsClient
 
         private async void Get_Trucks(object sender, EventArgs e)
         {
-            HttpClient client = new HttpClient();
-
-            var url = "http://192.168.0.218:5160/api/Values/Get_Trucks";
-
-            var response = await client.GetFromJsonAsync<List<Truck>>(url);
-
-            if (response != null)
+            try
             {
-                TrucksLabel.Text = string.Empty;
-                foreach (var truck in response)
+                HttpClient client = new HttpClient();
+
+                var url = "http://192.168.0.218:5160/api/Values/Get_Trucks";
+
+                var response = await client.GetFromJsonAsync<List<Truck>>(url);
+
+                if (response != null)
                 {
-                    TrucksLabel.Text += truck.Id + " " + truck.Owner + " " + truck.Capacity + " " + truck.IsBusy + "\n";
+                    TrucksLabel.Text = string.Empty;
+                    foreach (var truck in response)
+                    {
+                        TrucksLabel.Text += truck.Id + " " + truck.Owner + " " + truck.Capacity + " " + truck.IsBusy + "\n";
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                ErrorLabel.Text = ex.Message;
+                ErrorBorder.IsVisible = true;
+                ErrorButton.IsVisible = true;
+                ErrorButton.IsEnabled = true;
+            }
+            
 
         }
 
         private async void Login_Clicked(object sender, EventArgs e)
         {
-            HttpClient client = new HttpClient();
-
-            var url = "http://192.168.0.218:5160/api/Auth/Login";
-
-            var response = await client.PostAsJsonAsync(url, new { Username = Login_entry.Text.ToString(), Password = Password_entry.Text.ToString() });
-
-            if (response.IsSuccessStatusCode)
+            try
             {
-                Users user = await response.Content.ReadFromJsonAsync<Users>();
-                if (user != null)
-                {
-                    LoginResultLabel.Text = "Successfully logged in!";
+                HttpClient client = new HttpClient();
 
-                    await Shell.Current.GoToAsync($"{nameof(MainMenuPage)}?UserID={user.ID}");
+                var url = "http://192.168.0.218:5160/api/Auth/Login";
+
+                if(string.IsNullOrEmpty(Login_entry.Text) || string.IsNullOrEmpty(Password_entry.Text))
+                {
+                    LoginResultLabel.Text = "Please enter both username and password.";
+                    count++;
+                    if(count >= 10)
+                    {
+                        LoginResultLabel.Text = "stop spamming it you dumbass";
+                        count = 0;
+                    }
+                        return;
+                }
+
+                var response = await client.PostAsJsonAsync(url, new { Username = Login_entry.Text.ToString(), Password = Password_entry.Text.ToString() });
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Users user = await response.Content.ReadFromJsonAsync<Users>();
+                    if (user != null)
+                    {
+                        LoginResultLabel.Text = "Successfully logged in!";
+
+                        await Shell.Current.GoToAsync($"{nameof(MainMenuPage)}?UserID={user.ID}");
+                    }
+                }
+                else
+                {
+                    LoginResultLabel.Text = await response.Content.ReadAsStringAsync();
                 }
             }
-            else
+            catch (Exception ex)
             {
-                LoginResultLabel.Text = await response.Content.ReadAsStringAsync();
+                ErrorLabel.Text = ex.Message;
+                ErrorBorder.IsVisible = true;
+                ErrorButton.IsVisible = true;
+                ErrorButton.IsEnabled = true;
             }
+
+        }
+
+        private async void Close_Error(object sender, EventArgs e)
+        {
+            ErrorBorder.IsVisible = false;
+            ErrorButton.IsVisible = false;
+            ErrorButton.IsEnabled = false;
+            ErrorLabel.Text = string.Empty;
         }
     }
 }
