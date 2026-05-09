f784058a408ca0a81c8208735491ca5b5016b7fc
Nikodemus <nikodembilinski4@gmail.com>
2026-05-09 18:54:06 +0200

    feature: setup main section in generating invoice

--- Truck Logistics Server ASP.NET/Models/InvoiceDocument.cs
+++ Truck Logistics Server ASP.NET/Models/InvoiceDocument.cs
@@ -28,7 +28,6 @@ namespace TrucksLogisticsServerAPI.Models
                 page.Margin(40);
                 page.DefaultTextStyle(x => x.FontSize(11));
 
-
                 page.Header().Column(col =>
                 {
                     col.Item().Row(row =>
@@ -49,7 +48,45 @@ namespace TrucksLogisticsServerAPI.Models
                     col.Item().PaddingTop(10).LineHorizontal(1).LineColor(Colors.Grey.Lighten1);
                 });
 
-                page.Content();
+                // kolumna w kolumnieeeee crazy bro
+                page.Content().Column(col =>
+                {
+                    col.Item().PaddingTop(20).Column(col2 =>
+                    {
+                        col2.Item().Text("Job Details:").Bold().FontSize(12);
+                        col2.Item().PaddingTop(4).Text($"Job: {_invoice.Job.Name}");
+                        col2.Item().Text($"Company Name: {_invoice.Job.CompanyName}");
+                        col2.Item().Text($"Route: {_invoice.Job.LocationFrom} -> {_invoice.Job.LocationTo}");
+                        col2.Item().Text($"Description: {_invoice.Job.Description}");
+                    });
+
+                    col.Item().PaddingTop(20).LineHorizontal(1).LineColor(Colors.Grey.Lighten1);
+
+                    col.Item().PaddingTop(20).Table(table =>
+                    {
+                        table.ColumnsDefinition(tcol =>
+                        {
+                            tcol.RelativeColumn(4);
+                            tcol.RelativeColumn(2);
+                            tcol.RelativeColumn(2);
+                            tcol.RelativeColumn(2);
+                        });
+
+                        table.Header(header =>
+                        {
+                            header.Cell().Background(Colors.Grey.Lighten2).Padding(5).Text("Description").ExtraBold();
+                            header.Cell().Background(Colors.Grey.Lighten2).Padding(5).Text("Netto").ExtraBold();
+                            header.Cell().Background(Colors.Grey.Lighten2).Padding(5).Text($"VAT ({_invoice.VatRate}%)").ExtraBold();
+                            header.Cell().Background(Colors.Grey.Lighten2).Padding(5).Text("Brutto").ExtraBold();
+                        });
+
+                        var vatAmount = _invoice.GrossAmount - _invoice.NetAmount;
+                        table.Cell().Background(Colors.LightBlue.Medium).Padding(5).Text($"Transportation Service - {_invoice.Job.Name}");
+                        table.Cell().Background(Colors.LightBlue.Medium).Padding(5).Text($"{_invoice.NetAmount:F2} zł");
+                        table.Cell().Background(Colors.LightBlue.Medium).Padding(5).Text($"{vatAmount:F2} zł");
+                        table.Cell().Background(Colors.LightBlue.Medium).Padding(5).Text($"{_invoice.GrossAmount:F2} zł");
+                    });
+                });
                 page.Footer();
 
 
