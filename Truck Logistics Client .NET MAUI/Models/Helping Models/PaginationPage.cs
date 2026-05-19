88fea9b16d2eb109f8c24882f690232519e6ecc4
Nikodemus <nikodembilinski4@gmail.com>
2026-05-19 23:25:17 +0200

    created new classes for pagination, along with new get for users

---
+++ Truck Logistics Client .NET MAUI/Models/Helping Models/PaginationPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrucksLogisticsClient.Models.Helping_Models
+{
+    public class PaginationPage
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Truck Logistics Server ASP.NET/Models/Helping Models/PaginationPage.cs
@@ -0,0 +1,9 @@
+﻿namespace TrucksLogisticsServerAPI.Models.Helping_Models
+{
+    public class PaginationPage
+    {
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+    }
+}
