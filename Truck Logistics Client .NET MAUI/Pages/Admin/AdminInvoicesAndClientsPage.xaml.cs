db429bafdba4fe8ac13b2d5ca18984cd67aa36e1
Nikodemus <nikodembilinski4@gmail.com>
2026-05-31 23:50:21 +0200

    added getting proper values for blocks in invoices and clients overview

--- Truck Logistics Client .NET MAUI/Pages/Admin/AdminInvoicesAndClientsPage.xaml.cs
+++ Truck Logistics Client .NET MAUI/Pages/Admin/AdminInvoicesAndClientsPage.xaml.cs
@@ -48,6 +48,8 @@ public partial class AdminInvoicesAndClientsPage : ContentPage
         apiUrl = Preferences.Get("api_url", "127.0.0.1:5160/api/");
 
         await Get_Current_User();
+
+        await Get_Overview_Stats();
     }
     private async Task Get_Current_User()
     {
@@ -103,6 +105,33 @@ public partial class AdminInvoicesAndClientsPage : ContentPage
 
         Add_Invoice_Section.IsVisible = false;
         Add_Invoice_Section.IsEnabled = false;
+    }
+
+    private async Task Get_Overview_Stats()
+    {
+        var repsonse = await client.GetAsync(apiUrl + "Clients/Get_Clients_Stats");
+
+        if(repsonse.IsSuccessStatusCode) 
+        {
+            var stats = await repsonse.Content.ReadFromJsonAsync<ClientsStats>();
+            if(stats != null)
+            {
+                Total_Clients_Count.Text = stats.TotalClients.ToString();
+            }
+        }
+
+        var response = await client.GetAsync(apiUrl + "Invoices/Get_Invoices_Stats");
+        if (response.IsSuccessStatusCode)
+        {
+            var stats = await response.Content.ReadFromJsonAsync<InvoicesStats>();
+            if (stats != null)
+            {
+                
+                Total_Invoices_Count.Text = stats.Invoices_Count.ToString();
+                Unpaid_Invoices_Count.Text = stats.Unpaid_Count.ToString();
+                Overdue_Invoices_Count.Text = stats.Overdue_Count.ToString();
+            }
+        }
     }
 
     #region pagination
@@ -256,6 +285,8 @@ public partial class AdminInvoicesAndClientsPage : ContentPage
 
         Clients_Invoices_Overview.IsVisible = true;
         Clients_Invoices_Overview.IsEnabled = true;
+
+        await Get_Overview_Stats();
 
     }
     private async void Admin_Show_Clients_View(object sender, EventArgs e)
