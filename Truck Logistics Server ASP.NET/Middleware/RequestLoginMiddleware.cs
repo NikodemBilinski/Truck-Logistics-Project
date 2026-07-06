b2283905c3bb9fa04deb394120fa5c50f1a73c9c
Nikodemus <nikodembilinski4@gmail.com>
2026-07-06 18:54:02 +0200

    had some fun with creating new middleware for requestlogin

---
+++ Truck Logistics Server ASP.NET/Middleware/RequestLoginMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+namespace TrucksLogisticsServerAPI.Middleware
+{
+    public class RequestLoginMiddleware
+    {
+        private readonly ILogger _logger;
+        private readonly RequestDelegate _next;
+
+        public RequestLoginMiddleware(RequestDelegate next, ILogger<RequestLoginMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            if(context.Request.Path.StartsWithSegments("api/login"))
+            {
+                if(context.Request.Method != HttpMethods.Post)
+                {
+                    context.Response.StatusCode = 405; // Method Not Allowed
+                    return;
+                }
+                if(context.Connection.RemoteIpAddress != null)
+                {
+                    _logger.LogInformation($"Login attempt from IP: {context.Connection.RemoteIpAddress}");
+                }
+                if(context.Request.ContentType != "application/json")
+                {
+                    context.Response.StatusCode = 415; // Unsupported Media Type
+                    return;
+                }
+
+                _logger.LogInformation($"Login Request time took: {stopwatch.ElapsedMilliseconds} ms");
+                _logger.LogInformation("Login request is valid. Proceeding to next middleware.");
+                await _next(context);
+            }
+        }
+    }
+}
