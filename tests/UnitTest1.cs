426a23944f1efa4548b717887f72b7567e374b1c
Nikodemus <nikodembilinski4@gmail.com>
2026-07-07 21:42:59 +0200

    created basic unit test for adduser method in users controller

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,10 +1,62 @@
-﻿namespace tests;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using TrucksLogisticsServerAPI.Controllers;
+using TrucksLogisticsServerAPI.Data;
+using TrucksLogisticsServerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Microsoft.Identity.Client;
+using System.Diagnostics;
+namespace tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+
+    private DataContext GetDataContext()
     {
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseSqlServer("Server=.\\SQLEXPRESS;Database=TruckDB;Trusted_Connection=True;TrustServerCertificate=True;")
+            .Options;
+
+        return new DataContext(options);
+    }
+
+    
+
+    [Fact]
+    public async Task Test1()
+    {
+        // arrange
+
+        var user = new Users
+        {
+            FirstName = "jj",
+            LastName = "User",
+            Age = 30,
+            Username = "testuser",
+            Password = "password",
+            Role = "user"
+        };
+
+        var context = GetDataContext();
+
+        var userscontroller = new UsersController(context);
+
+        // act
+
+        var result = await userscontroller.AddUser(user);
+
+
+        // assert
+        
+        var saveduser = await context.Users.FirstOrDefaultAsync(u => u.Username == "testuser");
+
+        Assert.NotNull(saveduser);
+
+        //cleanup
+
+        context.Users.Remove(saveduser);
+        await context.SaveChangesAsync();
 
     }
 }
